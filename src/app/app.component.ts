3e451ef4a9b1bc615c00e4883266f6febdd932bb
Guillaume Grasset <guillaume.grasset@epita.fr>
2019-08-29 16:16:25 +0200

    Submission of the tp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog';
+  postsInit = [
+    new Post('Mon super post',
+      'Ceci est mon premier post et il est déjà chouette' ,
+      1),
+    new Post('La Terre est plate',
+      'Parce que, il faut se l\'avouer c\'est vrai',
+      -42),
+    new Post('Rien à dire',
+      'On est obligé de marquer quelquechose ?',
+      0)
+  ];
+}
+
+export class Post {
+  title: string;
+  content: string;
+  loveIts: number;
+  createdAt: Date;
+
+  constructor(title: string, content: string, loveIts: number) {
+    this.title = title;
+    this.content = content;
+    this.loveIts = loveIts;
+    this.createdAt = new Date();
+  }
 }
